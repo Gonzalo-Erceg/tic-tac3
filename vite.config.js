f77d1a95b4b6f72944edae42d647c445a388cc99
Gonzalo-Erceg <gonzalo.erceg@hotmail.com>
2024-01-27 09:58:26 -0300

    arreglando errores

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://gonzalo-erceg.github.io/tic-tac3"
+  base:"https://gonzalo-erceg.github.io/tic-tac3/"
 })
